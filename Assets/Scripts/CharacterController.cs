5ed0cc732a6566bfafb8422e333c80ba55e811bf
Micah Marino <36608767+glassedgamer@users.noreply.github.com>
2026-03-02 16:11:06 -0500

    Player Movement
    Player can switch between three different spots and glides smoothly between them

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    // The three positios that the player can switch between
+    public Vector3 leftPos;
+    public Vector3 centerPos;
+    public Vector3 rightPos;
+
+    /*
+     * currentSpot = 1 --> leftPos
+     * currentSpot = 2 --> centerPos
+     * currentSpot = 3 --> rightPos
+     */
+    public int currentSpot = 2;
+
+    public float forwardSpeed;
+    public float sideSpeed;
+
+    private void Start()
+    {
+        currentSpot = 2;
+    }
+
+    private void Update()
+    {
+        SwitchPosition();
+    }
+
+    void SwitchPosition()
+    {
+        // Determines which spot number you're on depending on which button you press
+        if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            currentSpot--;
+        } else if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            currentSpot++;
+        }
+
+        // Limits current spot so it can't go past 1 or 3
+        if (currentSpot < 1)
+            currentSpot = 1;
+        else if (currentSpot > 3)
+            currentSpot = 3;
+
+        // Takes spot number after button press and assigns a new position to the player
+        if (currentSpot == 1)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, leftPos, sideSpeed * Time.deltaTime);
+        } else if (currentSpot == 2)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, centerPos, sideSpeed * Time.deltaTime);
+        } else if (currentSpot == 3)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, rightPos, sideSpeed * Time.deltaTime);
+        }
+    }
+}
